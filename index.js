3f74af10ec0abc12aeefb8c7b41efbbe693790ff
HusbanShah97 <husbanshah97@gmail.com>
2024-07-20 16:12:23 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let isFreind = 'Husban';
+if (isFreind === 'Husban' || isFreind === 'Sarim') {
+    console.log(`${isFreind} is your freind`);
+}
+else {
+    console.log(`${isFreind} is not your freind`);
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+
+let isFreind: string = 'Husban';
+if (isFreind === 'Husban' || isFreind === 'Sarim') {
+    console.log(`${isFreind} is your freind`);
+    
+    
+} else {
+    console.log(`${isFreind} is not your freind`);
+    
+}
